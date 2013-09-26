e85c183c54c8439c9db1e4bed6374b2a777a811f
Luis García Gómez <garciagomezluis.94@gmail.com>
2013-09-26 19:42:30 -0300

    Practicas

--- Luis/Practico1/practico.hs
+++ Luis/Practico1/practico.hs
@@ -26,3 +26,35 @@
 	sumatoria2 n | n == 1		= -2
 	sumatoria2 n				= ( ( -1 )^n * 2^n ) + ( sumatoria2 ( n - 1 ) )
 	
+{-
+
+	3. Escribir una función que tome como parámetro dos listas de enteros, y que calcule la cantidad de elementos de la primera lista que se encuentran también en la segunda.
+
+	Tomo: A : [ 1..14 ] Entonces se calcula #( A Int B )
+		  B : [ 2..10 ]
+		  
+	Tomando a A y a B como conjuntos, si alguno de sus elementos se repite, estos se cuentan como uno solo para encontrar el cardinal del conjunto, de esta manera si tenemos:
+	
+		  A : [ 1,2,1,3 ]
+		  B : [ 1,3,2,1 ]
+		  --------------
+	A Int B : [ 1,2,3   ]			Y el resultado es : #( A Int B )
+
+-}
+
+	getInterseccion			 :: [ Int ] -> [ Int ] -> [ Int  ]
+	estaElementoEnConjunto	 :: [ Int ] ->   Int   ->   Bool
+	
+	getInterseccion 			[ ]		[ ] = [ ]
+	getInterseccion				 x		[ ] = [ ]
+	getInterseccion				[ ]		 x	= [ ]
+	
+	
+	
+	estaElementoEnConjunto		[ ]  x	= False
+	estaElementoEnConjunto	 (y:ys)  x  | y == x 		= True
+										| otherwise		= estaElementoEnConjunto ys x
+	
+	
+
+
