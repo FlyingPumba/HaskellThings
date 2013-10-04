bcc64771af329e2506b33dc75b8049438aba0aa2
Ivan Arcuschin <iarcuschin@gmail.com>
2013-10-04 00:11:52 -0300

    Finished 5th class exercises and MergeSort

--- Ivan/ejercicios-clase5.hs
+++ Ivan/ejercicios-clase5.hs
@@ -4,3 +4,21 @@ imparesHastaN 1 = [1]
 imparesHastaN 2 = [1]
 imparesHastaN n | mod n 2 == 0 = imparesHastaN (n-3)++[n-1]
 		| otherwise = imparesHastaN (n-2)++[n]
+
+listaSumatoria :: Int -> [Int]
+listaSumatoria 1 = [1]
+listaSumatoria n = (n+x):x:xs
+	where (x:xs) = listaSumatoria (n-1)
+
+mergeCore :: [Int] -> [Int] -> [Int]
+mergeCore ys [] = ys
+mergeCore [] xs = xs
+mergeCore (y:ys) (x:xs) | y <= x = y:(mergeCore ys (x:xs))
+						| otherwise = x:(mergeCore (y:ys) xs)
+
+mergeSort :: [Int] -> [Int]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort (x:xs) = mergeCore (mergeSort primera) (mergeSort segunda)
+	where (primera, segunda) = splitAt (div (length (x:xs)) 2) (x:xs)
+
