e9df54f2f41a7c0398416f4ff813612f9ce4fa8b
Ivan Arcuschin <iarcuschin@gmail.com>
2013-10-04 12:15:34 -0300

    Finished 7th class exercises

---
+++ Ivan/ejercicios-clase7.hs
@@ -0,0 +1,30 @@
+mcdEuclides :: Integer -> Integer -> Integer
+mcdEuclides a 0 = a
+mcdEuclides a b = mcdEuclides b (mod a b)
+
+mcdEuclidesCantPasos :: Integer -> Integer -> Integer
+mcdEuclidesCantPasos a 0 = 0
+mcdEuclidesCantPasos a b = 1 + mcdEuclidesCantPasos b (mod a b)
+
+euclidesFibonacci :: Integer -> Integer
+euclidesFibonacci n = mcdEuclidesCantPasos x y
+	where (x:y:xs) = sfib n
+
+sfib :: Integer -> [Integer]
+sfib 0 = [0]
+sfib 1 = [1, 0]
+sfib n = (x+y) : (x:y:fs)
+	where (x:y:fs) = sfib (n-1)
+
+base10 :: Integer -> [Integer] -> Integer
+base10 n [] = 0
+base10 n (x:xs) = (n^exponente)*x + base10 n xs
+	where exponente = (length (x:xs))-1
+
+base10lista :: Integer -> [Integer] -> [Integer]
+base10lista n [] = []
+base10lista n (x:xs) = convertirALista(base10 n (x:xs))
+
+convertirALista :: Integer -> [Integer]
+convertirALista 0 = []
+convertirALista n = (convertirALista (div n 10))++[mod n 10]
